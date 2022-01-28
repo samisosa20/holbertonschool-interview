712f563ea930513f9989b3dd5a590cf38d8b9c85
Sammy Guttman <1449@holbertonschool.com>
2022-01-28 06:54:38 -0500

    testing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,45 +1,33 @@
 #!/usr/bin/python3
-"""
-    Reads stdin line by line and computes metrics:
-    For every 10 lines:
-        - print the status number with the number of times it
-        appears
-        - print the sum of the file sizes
-"""
-if __name__ == "__main__":
-    import sys
-    import signal
+"""check stdin"""
+import sys
 
-    c = fileSize = 0
-    statCount = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-                 "404": 0, "405": 0, "500": 0}
 
-    def handleTen(statCount, fileSize):
-        print("File size: {}".format(fileSize))
-        for key in sorted(statCount.keys()):
-            if statCount[key] == 0:
-                continue
-            print("{}: {}".format(key, statCount[key]))
-
-    try:
-        for line in sys.stdin:
-            c += 1
-            split = line.split(" ")
-            try:
-                status = split[-2]
-                fileSize += int(split[-1])
-
-                if status in statCount:
-                    statCount[status] += 1
-            except Exception:
-                pass
-
-            if c % 10 == 0:
-                handleTen(statCount, fileSize)
-
-        else:
-            handleTen(statCount, fileSize)
-
-    except (KeyboardInterrupt, SystemExit):
-        handleTen(statCount, fileSize)
-        raise
+total_size = 0
+status = ['200', '301', '400', '401', '403', '404', '405', '500']
+times_status = [0, 0, 0, 0, 0, 0, 0, 0]
+counter = 0
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 2:
+            size = line_list[-1]
+            code = line_list[-2]
+            if code in status:
+                i = status.index(code)
+                times_status[i] += 1
+            total_size += int(size)
+            counter += 1
+        if counter == 10:
+            print("File size: {:d}".format(total_size))
+            for i in range(8):
+                if times_status[i] != 0:
+                    print("{:}: {:d}".format(status[i], times_status[i]))
+            counter = 0
+except Exception:
+    pass
+finally:
+    print("File size: {:d}".format(total_size))
+    for i in range(8):
+        if times_status[i] != 0:
+            print("{:}: {:d}".format(status[i], times_status[i]))
