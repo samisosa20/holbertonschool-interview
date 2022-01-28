3a8f3026d210edf3d9ffa22c0cbe93d3e1924a38
Sammy Guttman <1449@holbertonschool.com>
2022-01-28 06:51:23 -0500

    testing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,86 +1,48 @@
 #!/usr/bin/python3
 """
-0x06. Log Parsing, task 0. Log parsing
-Parses a log of HTTP GET request results from stdin to tabulate the total
-counts of status codes appearing in each response, and the total file size
-across all requests.
-Example of expected log line input:
-128.230.61.246 - [2017-02-05 23:31:23.258076] \
-"GET /projects/260 HTTP/1.1" 301 292
-Fields:
-<IP Address> - [<date>] "<GET request>" <response status code> <file size>
+Task - Script that reads stdin line by line and computes metrics
 """
 
-
-def print_log_totals(total_file_size, code_counts):
-    """
-    Prints current totals of file size and status code counts.
-    Args:
-        total_file_size (int): cumulative total of bytes received through
-            GET requests in log
-        code_counts (OrderedDict): totals of status codes from repsonses
-    """
-    print("File size: {}".format(total_file_size))
-    for code in code_counts:
-        if code_counts[code] > 0:
-            print("{}: {}".format(code, code_counts[code]))
+import sys
 
 
-if __name__ == '__main__':
-    from sys import argv, stdin, stderr
-    from collections import OrderedDict
-    from datetime import datetime
+if __name__ == "__main__":
+    st_code = {"200": 0,
+               "301": 0,
+               "400": 0,
+               "401": 0,
+               "403": 0,
+               "404": 0,
+               "405": 0,
+               "500": 0}
+    count = 1
+    file_size = 0
 
-    line_no = 0
-    total_file_size = 0
-    code_counts = OrderedDict.fromkeys([200, 301, 400, 401, 403,
-                                        404, 405, 500], 0)
+    def parse_line(line):
+        """ Read, parse and grab data"""
+        try:
+            parsed_line = line.split()
+            status_code = parsed_line[-2]
+            if status_code in st_code.keys():
+                st_code[status_code] += 1
+            return int(parsed_line[-1])
+        except Exception:
+            return 0
+
+    def print_stats():
+        """print stats in ascending order"""
+        print("File size: {}".format(file_size))
+        for key in sorted(st_code.keys()):
+            if st_code[key]:
+                print("{}: {}".format(key, st_code[key]))
 
     try:
-        for line in stdin:
-            line_no += 1
-
-            # both ip addresses/URLs allowed, but some lines may be raw text
-            a = line.split('-', 1)
-            if len(a) != 2:
-                # likely not a formatted line
-                continue
-
-            # checking timestamp
-            b = a[1].split(']')
-            timecode = b[0].lstrip(' [')
-            try:
-                datetime.strptime(timecode, '%Y-%m-%d %H:%M:%S.%f')
-            except:
-                stderr.write("{}: {}: invalid timecode\n".format(
-                    argv[0], line_no))
-                pass
-
-            # checking URL
-            c = b[1].split('"')
-            c = c[1:]
-            if c[0] != 'GET /projects/260 HTTP/1.1':
-                stderr.write("{}: {}: unexpected HTTP request\n".format(
-                    argv[0], line_no))
-
-            # prep for status code and file size
-            d = c[1].lstrip(' ')
-            d = d.rstrip('\n')
-            d = d.split(' ')
-
-            # checking status code (invalid codes skipped without error)
-            if d[0].isdecimal():
-                code = int(d[0])
-                code_counts[code] += 1
-
-            # checking file size (invalid sizes skipped without error)
-            if d[1].isdecimal():
-                total_file_size += int(d[1])
-
-            if line_no % 10 == 0:
-                print_log_totals(total_file_size, code_counts)
-        print_log_totals(total_file_size, code_counts)
-
-    except (KeyboardInterrupt):
-        print_log_totals(total_file_size, code_counts)
+        for line in sys.stdin:
+            file_size += parse_line(line)
+            if count % 10 == 0:
+                print_stats()
+            count += 1
+    except KeyboardInterrupt:
+        print_stats()
         raise
+    print_stats()
