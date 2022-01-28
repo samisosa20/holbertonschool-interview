19fd4712dccaf937a40364b78a2f88538d37b070
Sammy Guttman <1449@holbertonschool.com>
2022-01-28 06:46:18 -0500

    fix issue

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,47 +1,59 @@
 #!/usr/bin/python3
 """
-Module that parses a log and prints stats to stdout
+Script that reads stdin line by line and computes metrics:
+- Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
+                code> <file size>
+- After every 10 lines and/or a keyboard interruption (CTRL + C), print these
+    statistics from the beginning:
+Example:
+    File size: 5213
+    200: 2
+    401: 1
+    403: 2
+    404: 1
+    405: 1
+    500: 3
 """
-from sys import stdin
-
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-
-size = 0
+import sys
+stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0}
+summ = 0
 
 
-def print_stats():
-    """Prints the accumulated logs"""
-    print("File size: {}".format(size))
-    for status in sorted(status_codes.keys()):
-        if status_codes[status]:
-            print("{}: {}".format(status, status_codes[status]))
+def prn_stats():
+    """
+    Function that print stats about log
+    """
+    global summ
+
+    print('File size: {}'.format(summ))
+    stcdor = sorted(stcd.keys())
+    for each in stcdor:
+        if stcd[each] > 0:
+            print('{}: {}'.format(each, stcd[each]))
 
 
 if __name__ == "__main__":
-    count = 0
+    cnt = 0
     try:
-        for line in stdin:
+        """ Iter the standar input """
+        for data in sys.stdin:
             try:
-                items = line.split()
-                size += int(items[-1])
-                if items[-2] in status_codes:
-                    status_codes[items[-2]] += 1
+                fact = data.split(' ')
+                """ If there is a status code """
+                if fact[-2] in stcd:
+                    stcd[fact[-2]] += 1
+                """ If there is a lenght """
+                summ += int(fact[-1])
             except:
                 pass
-            if count == 9:
-                print_stats()
-                count = -1
-            count += 1
+            """ Printing control """
+            cnt += 1
+            if cnt == 10:
+                prn_stats()
+                cnt = 0
     except KeyboardInterrupt:
-        print_stats()
+        prn_stats()
         raise
-    print_stats()
+    else:
+        prn_stats()
